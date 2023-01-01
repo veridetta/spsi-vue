154c5487e9360668ae9aa7020e5a25d702e76768
Vr <veridetta@gmail.com>
2023-01-02 03:13:52 +0700

    footer

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -5,8 +5,7 @@
     <span class="float-md-start d-block d-md-inline-block mt-25">COPYRIGHT &copy;
       <script>
         document.write(new Date().getFullYear())
-      </script><a class="ms-25" href="https://1.envato.market/pixinvent_portfolio"
-        target="_blank">Pixinvent</a>,
+      </script>
       <span class="d-none d-sm-inline-block">All rights Reserved</span>
     </span>
     <span class="float-md-end d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span>
